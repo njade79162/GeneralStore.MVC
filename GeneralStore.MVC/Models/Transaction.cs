f30d34e53ac78403bd2c0bd762ab2dd0c5909c66
Jade Neff <njade79162@gmail.com>
2022-01-20 19:47:02 -0500

    Add project files.

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [Display(Name = "Customer")]
+        public virtual int CustomerID { get; set; }
+        
+        [ForeignKey("CustomerID")]
+        public virtual Customer Customer { get; set; }
+
+        [Display(Name = "Product")]
+        public virtual int ProductID { get; set; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
+    }
+}
